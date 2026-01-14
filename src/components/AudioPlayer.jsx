62f21d90fe1a572cbdf0037f84c16fc11e44d655
Joaquin Hoz <joaquinnhoz1@gmail.com>
2026-01-14 17:15:11 -0300

    volumen

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -11,7 +11,7 @@ export default function AudioPlayer() {
     if (!audio) return;
 
     audio.loop = true;
-    audio.volume = 0.35;
+    audio.volume = 0.10;
     audio.muted = true;
 
     const tryPlay = async () => {
